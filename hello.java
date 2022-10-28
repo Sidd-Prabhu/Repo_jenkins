389721dbc12fdd95d25476751eaead7aab94468a
Sidd-Prabhu <114066700+Sidd-Prabhu@users.noreply.github.com>
2022-10-29 00:37:36 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,6 +3,6 @@ class HelloWorld
 {
     public static void main(String args[])
     {
-        System.out.println("Hello World, Jenkins...........!!!!!!!");
+        System.out.println("Hello World");
     }
 }
